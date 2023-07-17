2611f2a935aa39392cc33bba59b1a3a9fc029a6d
Brettney Chau-Dang <bchaudang@gmail.com>
2023-07-17 18:43:06 -0400

    demo divbox

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,8 +1,8 @@
-function init() {
-    // dropdown menu reference
+  function init() {
+    // dropdown menu reference selector
     var selector = d3.select("#selDataset");
   
-    //  Fetch the JSON data and console log it
+    // Fetch the JSON data
     d3.json("samples.json").then((data) => {
   
       var sampleNameId = data.names;
@@ -25,9 +25,31 @@ function init() {
   init();
   
   function optionChanged(newSampleId) {
-    // Fetch new data each time a new test ID sample is selected
+    // Fetch new data each time a new test ID sample id is selected
     buildMetadata(newSampleId);
     buildCharts(newSampleId);
     
+  }
+  
+  // Demographics Info divbox 
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      // Find the info for the selected test ID sample
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      // Use d3 library to select the panel with id of `#sample-metadata`
+      var PANEL = d3.select("#sample-metadata");
+  
+      // Use `.html("") to clear any existing metadata
+      PANEL.html("");
+  
+      // Use `Object.entries` to add each key and value pair to the panel
+      
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      });
+  
+    });
   }
   
