802d762ff30205abc147eb3fea41cc9877543733
Brettney Chau-Dang <bchaudang@gmail.com>
2023-07-17 19:08:16 -0400

    building functions

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,7 +2,7 @@
     // dropdown menu reference selector
     var selector = d3.select("#selDataset");
   
-    // Fetch the JSON data
+    // fetch the JSON data, use variables
     d3.json("samples.json").then((data) => {
   
       var sampleNameId = data.names;
@@ -14,42 +14,55 @@
           .property("value", sample);
       });
   
-      // Use the first test ID sample from the list to build the initial plots
+      // use the first test ID sample from the list to build the initial plots
       var firstSampleId = sampleNames[0];
       buildCharts(firstSampleId);
       buildMetadata(firstSampleId);
     });
   }
   
-  // Initialize the dashboard
+  // initialize the dashboard
   init();
   
   function optionChanged(newSampleId) {
-    // Fetch new data each time a new test ID sample id is selected
+    // fetch new data each time a new test ID sample id is selected
     buildMetadata(newSampleId);
     buildCharts(newSampleId);
     
   }
   
-  // Demographics Info divbox 
+  // demographics info divbox panel, use variables
   function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
-      var metadata = data.metadata;
-      // Find the info for the selected test ID sample
+      var metadata = data.metadata;      
       var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
       var result = resultArray[0];
-      // Use d3 library to select the panel with id of `#sample-metadata`
       var PANEL = d3.select("#sample-metadata");
   
-      // Use `.html("") to clear any existing metadata
+      // clear the panel for existing metadate with `.html("") 
       PANEL.html("");
   
-      // Use `Object.entries` to add each key and value pair to the panel
-      
-      Object.entries(result).forEach(([key, value]) => {
+      // add each key and value pair to the panel using `Object.entries` 
+            Object.entries(result).forEach(([key, value]) => {
         PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
       });
   
     });
   }
-  
+
+
+    // use buildCharts function, use console to log data, use variables
+    function buildCharts(sample) {
+      d3.json("samples.json").then((data) => {
+        console.log(data);
+        var samples = data.samples;
+        var resultArray = samples.filter(obj => obj.id == sample);
+        var result = resultArray[0];
+        
+
+      })
+
+
+
+
+    }
